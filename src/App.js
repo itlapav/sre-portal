243e3b1168d21bf46042fc189169220493317731
itlapav <itlap983@gmail.com>
2025-12-26 23:54:16 +0530

    update the cicd workflow

--- src/App.js
+++ src/App.js
@@ -18,15 +18,15 @@ function App() {
         <p>LF Service Reliability Overview</p>
       </header>
 
-      {incident && (
+      {/* {incident && (
         <div className="incident">
           🚨 Incident ongoing — team investigating
         </div>
-      )}
+      )} */}
 
-      <button className="toggle" onClick={() => setIncident(!incident)}>
+      {/* <button className="toggle" onClick={() => setIncident(!incident)}>
         {incident ? "Resolve Incident" : "Trigger Incident"}
-      </button>
+      </button> */}
 
       <div className="services">
         {services.map((service, index) => (
